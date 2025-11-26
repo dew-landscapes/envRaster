7bee33d401e087c4bd8b3d28add340adef8ffd5a
Acanthiza <nigel.willoughby@sa.gov.au>
2025-11-27 06:51:49 +1030

    distance instead of distance_to

--- data-raw/distance.R
+++ data-raw/distance.R
@@ -1,4 +1,4 @@
 distance <- tibble::tribble(
   ~source, ~layer, ~description, ~units, ~indicates, ~data_ref,
-  "ABS", "coast", "distance to coast", "metres", "exposure to factors like salt spray and wind", "https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/access-and-downloads/digital-boundary-files#downloads-for-gda2020-digital-boundary-files",
+  "ABS", "coast", "distance", "metres", "exposure to factors like salt spray and wind", "https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/access-and-downloads/digital-boundary-files#downloads-for-gda2020-digital-boundary-files",
   )

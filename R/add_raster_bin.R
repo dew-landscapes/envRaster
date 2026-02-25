562cd175deb01f4653aa2167781dcf0bf6ebd4da
Acanthiza <nigel.willoughby@sa.gov.au>
2026-02-25 16:58:35 +1030

    soft deprecate add_raster_cell. New function add_raster_bin better reflects how this function is mostly used. Also close #3.

--- R/add_raster_bin.R
+++ R/add_raster_bin.R
@@ -1,6 +1,8 @@
 
-#' Add cell to df with lat/long
+#' Add 'bin' to dataframe with coordinates
 #'
+#' Direct descendant of `envRaster::add_raster_cell()`. Adding `cell` is now
+#' optional (and defaults to `FALSE`).
 #'
 #' @param ras SpatRaster, or path to raster (needs to be path if running in
 #' parallel), with cell numbers to extract
@@ -10,38 +12,34 @@
 #' @param add_xy Logical. Generate (centroid) x and y coords from cell? If TRUE,
 #' these will be returned as `cell_x` and `cell_y` where 'x' and 'y' are as
 #' defined by arguments `x` and `y`.
+#' @param add_cell Logical. Add the cell id to the data frame?
 #' @param return_old_xy Logical. If true, the original x and y values will be
-#' returned as `old_x` and `old_y` where 'x' and 'y' are as defined by arguments
-#'  `x` and `y`.
+#' returned as `old_x` and `old_y` where 'x' and 'y' are as per arguments `x`
+#' and `y`.
 #' @param add_val Logical. If true the value(s) for cell will be extracted using
 #' `terra::extract()`.The names of any columns resulting from `add_val` will be
 #' the same as the names in `ras`.
-#' @param force_new Logical. If there is already a column `cell` in `df`, remove
-#' it first?
 #'
-#' @return `df` with additional column `cell` with cell numbers from ras.
-#' Depending on the value of `add_xy` and `return_old_xy`, extra columns for
-#' centroid x and y values and old x and y values respetively. If `add_val` any
-#' values from `ras`. Note that, irrespective of the crs of `ras`,
-#' any returned x and y values will be in `crs_df`.
+#' @return `df` (as tibble) with additional column(s):
+#' * `cell` with cell numbers from `ras`, if `add_cell`
+#' * `cell_[x]` and `cell_[y]`, if `add_xy`
+#' * If `add_val` any values from `ras`
+#' Irrespective of the crs of `ras`, any returned x and y values will be in
+#' `crs_df`.  If `return_old_xy` is `FALSE`, the original `x` and `y` columns
+#' will be dropped.
 #' @export
 #'
-#' @example inst/examples/add_raster_cell_ex.R
-add_raster_cell <- function(ras
+#' @example inst/examples/add_raster_bin_ex.R
+add_raster_bin <- function(ras
                             , df
                             , x = "long"
                             , y = "lat"
                             , crs_df = 4326
-                            , add_xy = FALSE
+                            , add_xy = TRUE
+                            , add_cell = ! add_xy
                             , return_old_xy = FALSE
                             , add_val = FALSE
-                            , force_new = TRUE
                             ) {
-
-  lifecycle::deprecate_soft("2025-02-25"
-                            , "add_raster_cell()"
-                            , with = "add_raster_bin()"
-                            )
 
   if(! "SpatRaster" %in% class(ras)) ras <- terra::rast(ras)
 
@@ -53,8 +51,7 @@ add_raster_cell <- function(ras
   df <- df %>%
     dplyr::rename(!!rlang::ensym(old_x) := !!rlang::ensym(x)
                   , !!rlang::ensym(old_y) := !!rlang::ensym(y)
-                  ) %>%
-    {if(force_new) (.) %>% dplyr::select(!tidyselect::matches("^cell$")) else (.)}
+                  )
 
   df_xy <- df %>%
     dplyr::distinct(!!rlang::ensym(old_x)
@@ -67,16 +64,14 @@ add_raster_cell <- function(ras
                    , !!rlang::ensym(old_y)
                    )
 
-  points <- df_xy %>%
-    sf::st_as_sf(coords = c(old_x, old_y)
-                 , crs = crs_df
-                 , remove = FALSE
-                 ) %>%
-    sf::st_transform(crs = terra::crs(ras)) %>%
-    dplyr::bind_cols(tibble::as_tibble(sf::st_coordinates(.)) %>%
-                       dplyr::rename(old_x_ras = X, old_y_ras = Y)
-                     ) %>%
-    sf::st_set_geometry(NULL)
+  points <- df_xy |>
+    envFunc::project_df(x = "old_X"
+                        , y = "old_Y"
+                        , new_x = "old_x_ras"
+                        , new_y = "old_y_ras"
+                        , crs_from = paste0("epsg:", crs_df)
+                        , crs_to = paste0("epsg:", terra::crs(ras, describe = TRUE)$code)
+                        )
 
   cells <- terra::cellFromXY(ras
                              , as.matrix(points[c("old_x_ras", "old_y_ras")])
@@ -92,21 +87,17 @@ add_raster_cell <- function(ras
 
     xy_res <- terra::xyFromCell(ras
                                 , cells
-                                ) %>%
-      tibble::as_tibble() %>%
-      dplyr::bind_cols(cell = cells) %>%
-      dplyr::filter(!is.na(x)
-                    , !is.na(y)
-                    ) %>%
-      sf::st_as_sf(coords = c("x","y")
-                   , crs = terra::crs(ras)
-                   ) %>%
-      sf::st_transform(crs = crs_df) %>%
-      dplyr::mutate(!!rlang::ensym(new_x) := sf::st_coordinates(.)[,1]
-                    , !!rlang::ensym(new_y) := sf::st_coordinates(.)[,2]
-                    ) %>%
-      sf::st_set_geometry(NULL) %>%
-      unique()
+                                ) |>
+      tibble::as_tibble() |>
+      envFunc::project_df(x = "x"
+                          , y = "y"
+                          , new_x = !!rlang::ensym(new_x)
+                          , new_y = !!rlang::ensym(new_y)
+                          , crs_from = paste0("epsg:", terra::crs(ras, describe = TRUE)$code)
+                          , crs_to = paste0("epsg:", crs_df)
+                          ) |>
+      dplyr::select(- x, - y) |>
+      dplyr::distinct()
 
     res <- merge(res, xy_res) %>%
       tibble::as_tibble()
@@ -129,10 +120,13 @@ add_raster_cell <- function(ras
   res <- df %>%
     dplyr::left_join(res) %>%
     dplyr::select(cell
+                  , tidyselect::any_of(c(new_x, new_y))
                   , everything()
                   , -tidyselect::matches("old.*ras")
                   ) %>%
-    {if(!return_old_xy) (.) %>% dplyr::select(-tidyselect::contains("old")) else (.)} %>%
+    {if(! return_old_xy) (.) %>% dplyr::select(-tidyselect::contains("old")) else (.)} %>%
+    {if(! add_cell) (.) %>% dplyr::select(- cell) else (.)} %>%
+    {if(! add_xy) (.) %>% dplyr::select(- tidyselect::any_of(c(new_x, new_y))) else (.)} %>%
     tibble::as_tibble()
 
   return(res)
---
+++ inst/examples/add_raster_bin_ex.R
@@ -0,0 +1,64 @@
+
+  #library(envRaster)
+
+  out_dir <- file.path(system.file(package = "envRaster"), "examples")
+
+  base_file <- fs::path(out_dir, "cube", "base.tif")
+
+  if(!file.exists(base_file)) {
+
+    make_base_grid(aoi
+                   , out_res = 30
+                   , out_epsg = 8059
+                   , out_file = base_file
+                   , datatype = "INT1U"
+                   , overwrite = TRUE
+                   )
+  }
+
+  base <- terra::rast(base_file)
+
+  # random sample of points within the aoi
+  sample <- matrix(nrow = 2, ncol = 2) %>% # include some NA x and y values
+    rbind(sf::st_sample(aoi, 100) %>%
+            sf::st_transform(crs = 4326) %>%
+            sf::st_coordinates()
+          ) %>%
+    tibble::as_tibble() %>%
+    dplyr::mutate(attribute = sample(letters[1:10], nrow(.), replace = TRUE))
+
+  # add just cell centroids to sample
+  add_raster_bin(ras = base
+                 , df = sample
+                 , x = "X"
+                 , y = "Y"
+                 )
+
+  # add cell and cell centroids to sample
+  add_raster_bin(base
+                 , sample
+                 , add_cell = TRUE
+                 , x = "X"
+                 , y = "Y"
+                 )
+
+  # add cell, cell centroids and original 'x' and 'y' to sample
+  add_raster_bin(base
+                 , sample
+                 , add_cell = TRUE
+                 , return_old_xy = TRUE
+                 , x = "X"
+                 , y = "Y"
+                 )
+
+  # add cell, cell centroids, cell values and original 'x' and 'y' to sample
+  add_raster_bin(base
+                  , sample
+                  , add_cell = TRUE
+                  , return_old_xy = TRUE
+                  , add_val = TRUE
+                  , x = "X"
+                  , y = "Y"
+                  )
+
+

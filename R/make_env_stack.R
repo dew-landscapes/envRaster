c466a0782d5088fbb7eb44569b89180d714f6997
acanthiza <nigel.willoughby@sa.gov.au>
2026-03-18 20:59:52 +1030

    make_env_stack changes. Don't try to match extent (but warn about dropping rasters that don't match); exclude_list argument added.

--- R/make_env_stack.R
+++ R/make_env_stack.R
@@ -1,6 +1,6 @@
 #' Make a spatRaster from a vector of paths
 #'
-#' Allows for some degree of matching extent; settings a window (using the aoi);
+#' Allows for: setting a window (using the aoi);
 #' and recoding categorical raster(s) to no more than cat_pred_levels.
 #'
 #' @param predictors Character. Paths to rasters.
@@ -9,6 +9,12 @@
 #' @param aoi sf. Used in `terra::window()`
 #' @param cat_pred_levels Named list. Usually `prep$cat_pred_levels()` resulting
 #' from `envSDM::prep_sdm()`
+#' @param exclude_list Named list used to exclude rasters. If `is_env_pred`
+#' this can use any columns returned by
+#' `envRaster::parse_env_tif(tibble::tibble(path = predictors), parse = TRUE)`
+#' by specifying the column name and a regex to exclude within that column
+#' (filter). If not `is_env_pred` then any elements with `exclude_list` are
+#' excluded using grepl on only the file name (not the full path).
 #'
 #' @returns spatRaster
 #' @export
@@ -18,7 +24,19 @@ make_env_stack <- function(predictors
                            , is_env_pred = TRUE
                            , aoi = NULL
                            , cat_pred_levels = NULL
+                           , exclude_list = NULL
                            ) {
+
+  # if not using env naming, easier to grepl the exclude list here
+  if(all(! is_env_pred, !is.null(exclude_list))) {
+
+    predictors <- purrr::map(exclude_list
+                             , \(x) predictors[! grepl(paste0(x, collapse = "|"), basename(predictors))]
+                             ) |>
+      unlist() |>
+      unname()
+
+  }
 
   stack_desc <- predictors |>
     tibble::enframe(name = NULL, value = "path") |>
@@ -41,25 +59,59 @@ make_env_stack <- function(predictors
 
    if(nrow(unstackable)) {
 
-     warning(paste0(unstackable$path, collapse = "\n")
+     warning(paste0(unstackable$path, collapse = " \n")
              , "\n"
              , if(nrow(unstackable) > 1) " are " else " is "
              , "not stackable with the other "
              , nrow(stack_desc) - nrow(unstackable)
-             , " layers. Specified layers will not be used"
+             , " layers and will not be used"
              )
 
    }
 
-  stack <- stack_desc |>
+  predictors <- stack_desc |>
     dplyr::anti_join(unstackable) |>
-    dplyr::pull(path) |>
-    terra::rast()
+    dplyr::pull(path)
 
   if(is_env_pred) {
 
-    names(stack) <- envRaster::name_env_tif(tibble::tibble(path = predictors[! predictors %in% unstackable$path]), parse = TRUE) |>
+    ras_tib <- envRaster::name_env_tif(tibble::tibble(path = predictors)
+                                       , parse = TRUE
+                                       )
+
+    # if using env naming, use the returned tibble to filter the exclude_list
+    if(!is.null(exclude_list)) {
+
+      filter_func <- function(df, named_list) {
+
+        filter_col <- names(named_list)
+
+        df  |>
+          dplyr::filter(! grepl(paste0(named_list, collapse = "|")
+                                , !!rlang::ensym(filter_col)
+                                )
+                        )
+
+      }
+
+      for(i in 1:length(exclude_list)) {
+
+        ras_tib <- filter_func(ras_tib
+                               , exclude_list[i]
+                               )
+
+      }
+
+    }
+
+    stack <- terra::rast(ras_tib$path)
+
+    names(stack) <- ras_tib |>
       dplyr::pull(name)
+
+  } else {
+
+    stack <- terra::rast(predictors)
 
   }
 
@@ -76,7 +128,7 @@ make_env_stack <- function(predictors
 
     }
 
-    terra::window(stack) <- terra::ext(aoi |> sf::st_transform(crs = sf::st_crs(stack)))
+    terra::window(stack) <- terra::ext(aoi)
 
   }
 

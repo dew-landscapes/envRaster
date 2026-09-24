1da4039b96b2218ace9fdcdf16eb6453cc8eba58
acanthiza <nigel.willoughby@sa.gov.au>
2026-09-25 06:30:56 +0930

    bio04 scale and offset

--- data-raw/bioclim.R
+++ data-raw/bioclim.R
@@ -5,7 +5,7 @@ bioclim <- tibble::tribble(
   "bio01", "Annual Mean Temperature", "dec C", "annual trends", 0.001602222, 2.5,
   "bio02", "Mean Diurnal Range (Mean of monthly (max temp - min temp))", "dec C", "seasonality", 0.0007629627, 25,
   "bio03", "Isothermality (bio02 / bio07) (× 100)", NA, "seasonality", 0.001525925, 50,
-  "bio04", "Temperature Seasonality (standard deviation × 100)", NA, "seasonality", 0.007629627, 250,
+  "bio04", "Temperature Seasonality (standard deviation × 100)", NA, "seasonality", 0.07629627, 2500,
   "bio05", "Max Temperature of Warmest Month", "dec C", "extreme or limiting environmental factors", 0.001602222, 2.5,
   "bio06", "Min Temperature of Coldest Month", "dec C", "extreme or limiting environmental factors", 0.001602222, 2.5,
   "bio07", "Temperature Annual Range (bio05 - bio06)", "dec C", "annual trends", 0.001602222, 2.5,
